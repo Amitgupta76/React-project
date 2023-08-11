7011319da7e8775038f577f070416ee6c0424fab
Amit Gupta <amit.cs20@bmsce.ac.in>
2023-08-11 11:03:31 +0530

    React app with GraphQL integrated

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import InputForm from '../Components/InputForm';
 import DisplayInput from '../Components/DisplayInput';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_DATA, ADD_DATA } from '../graphql-queries';
 
 const Home = () => {
   const [inputArray, setInputArray] = useState<string[]>([]);
 
+  const { loading, error, data } = useQuery(GET_DATA);
+  const [addDataMutation] = useMutation(ADD_DATA);
+
+
   const handleInputSubmit = (input: string) => {
     setInputArray([...inputArray, input]);
+    addDataMutation({ variables: { data: input } });
   };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.log(error)
+    return <p>{error.message}</p>
+  }
+
 
   return (
     <div className="App" style={{textAlign: 'center'}}>
       <h1>Messages App</h1>
       <InputForm onInputSubmit={handleInputSubmit} />
-      <DisplayInput inputs={inputArray} />
+      <DisplayInput inputs={data.getData}/>
     </div>
   );
 }
